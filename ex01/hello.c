7e01d6722da11155373b629c807d7f602d718cb4
Jaiu06 <13728195051@163.com>
2026-06-04 14:17:00 +0800

    第二次提交：增加用户输入功能，更新README

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input[100];
+    printf("Hello World!\n");
+    printf("请输入一些内容: ");
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        size_t len = strlen(input);
+        if (len > 0 && input[len-1] == '\n') {
+            input[len-1] = '\0';
+        }
+        printf("你输入的是: %s\n", input);
+    } else {
+        printf("读取输入时发生错误。\n");
+    }
+    
     return 0;
 }
